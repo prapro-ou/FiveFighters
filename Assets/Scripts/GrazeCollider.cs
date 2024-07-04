0c2bfad5cd1d4f33496100cea58822f974315e53
koh <koh@progate.com>
2024-07-04 17:33:35 +0900

    GrazeCounterの仮実装

--- Assets/Scripts/GrazeCollider.cs
+++ Assets/Scripts/GrazeCollider.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class GrazeCollider : MonoBehaviour
 {
+    [SerializeField]
+    private Player _player;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _player.GrazeCounter += 1;
     }
 }

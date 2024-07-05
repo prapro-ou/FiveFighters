407eccb012a19bc69deff983582aaa3beb9bcb34
AT_NotePC <pcsa3uto@s.okayama-u.ac.jp>
2024-07-05 17:45:24 +0900

    弾消し能力のため、DestroyFieldプレハブを追加。ついでに弾のプレハブも追加。当たり判定のレイヤー関係を設定。

---
+++ Assets/Scripts/DestroyField.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyField : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        //ここに破壊する処理を書く
+    }
+}

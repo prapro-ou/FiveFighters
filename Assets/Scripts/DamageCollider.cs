23a279c0528161e7e7441813a8b7ef89b36efebf
AT_NotePC <pcsa3uto@s.okayama-u.ac.jp>
2024-07-07 22:12:09 +0900

    無敵時間のため、_inInvincibleフィールド、InInvincibleプロパティ、_invincibleTimeフィールド、InvincibleTimeプロパティを定義。OnTriggerEnter2D内の内容を変更。動作するかはわからない。

--- Assets/Scripts/DamageCollider.cs
+++ Assets/Scripts/DamageCollider.cs
@@ -7,6 +7,22 @@ public class DamageCollider : MonoBehaviour
     [SerializeField]
     private Player _player;
 
+    private bool _inInvincible;
+
+    public bool InInvincible
+    {
+        get {return _inInvincible;}
+        set {_inInvincible = value;}
+    }
+
+    private float _invincibleTime;
+
+    public float InvincibleTime
+    {
+        get {return _invincibleTime;}
+        set {_invincibleTime = value;}
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +37,21 @@ public class DamageCollider : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collider)
     {
+        if(InInvincible) {return;}
+
         EnemyBullet enemyBullet = collider.gameObject.GetComponent<EnemyBullet>();
+
+        _player.TakeDamage(enemyBullet.DamageValue);
+
+        StartCoroutine("StartInvincible", InvincibleTime);
+    }
+
+    private IEnumerator StartInvincible(float time)
+    {
+        InInvincible = true;
+
+        yield return new WaitForSeconds(time);
+
+        InInvincible = false;
     }
 }

cdac98d7164622310f3e0c286c519c4c4503bbf7
AT_NotePC <pcsa3uto@s.okayama-u.ac.jp>
2024-07-07 21:56:12 +0900

    _damageValueフィールド、DamageValueプロパティを追加。

--- Assets/Scripts/EnemyBullet.cs
+++ Assets/Scripts/EnemyBullet.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class EnemyBullet : MonoBehaviour
 {
+    [SerializeField]
+    private int _damageValue;
+
+    public int DamageValue
+    {
+        get {return _damageValue;}
+        set {_damageValue = value;}
+    }
+
     // Start is called before the first frame update
     void Start()
     {

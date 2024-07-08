f89bc438fc929fad1cd120db44921825b7709147
AT_NotePC <pcsa3uto@s.okayama-u.ac.jp>
2024-07-08 15:02:52 +0900

    PlayerがEnemyのオブジェクトをもっておくため、_currentEnemyフィールド、CurrentEnemyプロパティを定義。Enemy_Hexagonが出現時、プレイヤーのCurrentEnemyを更新するようにStartを変更。

--- Assets/Scripts/Enemies/Enemy_Hexagon.cs
+++ Assets/Scripts/Enemies/Enemy_Hexagon.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Enemy_Hexagon : Enemy
 {
+    private Player _player;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _player = GameObject.Find("Player").GetComponent<Player>();
+        _player.CurrentEnemy = this;
     }
 
     // Update is called once per frame

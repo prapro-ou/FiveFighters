7f5c29c68ca3a31d58826855fc05e643d2890c97
AT_NotePC <pcsa3uto@s.okayama-u.ac.jp>
2024-07-12 17:10:15 +0900

    MainSceneのHpBarにSliderをアタッチ Start内のUpdateHpを削除（更新順でエラーが起こってそうだったので）

--- Assets/Scripts/PlayerHpBar.cs
+++ Assets/Scripts/PlayerHpBar.cs
@@ -14,7 +14,7 @@ public class PlayerHpBar : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        UpdateHp();
+
     }
 
     // Update is called once per frame

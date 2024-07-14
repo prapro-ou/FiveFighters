865af6b48c3533bdf61ec2e66440ca2ffe33a6d1
AT_NotePC <pcsa3uto@s.okayama-u.ac.jp>
2024-07-14 12:27:47 +0900

    Playerの_Submitメソッドを編集。 UIObject型を抽象型に変更。InvoceUIActionメソッドを追加。

--- Assets/Scripts/UIObjects/UIObject.cs
+++ Assets/Scripts/UIObjects/UIObject.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UIObject : MonoBehaviour
+public abstract class UIObject : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -15,4 +15,6 @@ public class UIObject : MonoBehaviour
     {
         
     }
+
+    public abstract void InvokeUIAction();
 }

a14d297425e838ed43589deb13b57566742d1ae6
ryunosuke0115 <pyp70bok@s.okayama-u.ac.jp>
2024-07-19 14:46:18 +0900

    SpecialCircleSkillに拡大のAnimationを追加するため，SpecialCirclleExplode.anim，SpecialCirclleExplode.controllerを作成した．

--- Assets/Scripts/SpecialCircleExplode.cs
+++ Assets/Scripts/SpecialCircleExplode.cs
@@ -4,16 +4,17 @@ using UnityEngine;
 
 public class SpecialCircleExplode : MonoBehaviour
 {
+    private Animator _explodeAnim;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _explodeAnim = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _explodeAnim.SetBool("blExplode", true);
     }
 
     void OnTriggerEnter2D(Collider2D collider)
